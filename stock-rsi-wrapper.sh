bc29dc6a2746349d38e31802f3307c98b8190114
Parker Johnson <pjohn07@gmail.com>
2020-11-02 14:26:27 -0500

    adding logic to only run this between 9-10 EST, otherwise I wanna boot and not bork the DB.

--- stock-rsi-wrapper.sh
+++ stock-rsi-wrapper.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sleep 20
+HOUR=$(date +%H)
 
-source ./config.sh
+#I only want to run this when my azuer vm boots M-F just before market opens.
+#Othrwise I want my VM to not run this when I want to do DB maintenance.
+if (($HOUR >= 9 && $HOUR < 10)); then
+  sleep 20
+  source ./config.sh
+  /home/$MYHOME/python-analyze-stock-market/stock-rsi.py /home/$MYHOME/python-analyze-stock-market/symbols | grep -v completed | mail -s "Daily RSI" $MYEMAIL
 
-/home/$MYHOME/python-analyze-stock-market/stock-rsi.py /home/$MYHOME/python-analyze-stock-market/symbols | grep -v completed | mail -s "Daily RSI" $MYEMAIL
+fi
